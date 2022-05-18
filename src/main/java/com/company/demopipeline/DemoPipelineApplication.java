978409577e0c2707d94239e998d199c866ab05a0
TONOYAN SERGEY <A776517>
2022-05-18 19:24:11 +0400

    initial

---
+++ src/main/java/com/company/demopipeline/DemoPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.company.demopipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/demopipeline/DemoPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.demopipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
